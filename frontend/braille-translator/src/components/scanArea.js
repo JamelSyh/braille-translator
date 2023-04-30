6bef49e25e347bb2528faf40d1310766e23af02d
JamelSyh <djamelmessi988@gmail.com>
2023-04-30 07:45:18 +0100

    start working on the scan page

---
+++ frontend/braille-translator/src/components/scanArea.js
@@ -0,0 +1,16 @@
+import '../Scan.css'
+
+
+function ScanArea() {
+  return (
+
+    <div className="scanArea">
+      <div className="card input-wrapper">
+        <div className="text-area">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ScanArea;
--- frontend/braille-translator/src/pages/scan.js
+++ frontend/braille-translator/src/pages/scan.js
@@ -1,8 +1,50 @@
-
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { switchOption, switchLang, switchText, mobile } from "../redux/actions";
+import InputTextArea from '../components/inputTextArea';
+import OutputTextArea from '../components/outputTextArea';
+import ScanArea from "../components/scanArea";
+// import Keyboard from '../components/keyboard';
+// import BrailleBoard from '../components/brailleBoard';
+import '../App.css';
+import '../Scan.css';
 
 function Scan() {
+
+  const dispatch = useDispatch();
+  // const kb = useSelector(state => state.functions.keyboard);
+  // const brailleBoard = useSelector(state => state.functions.board);
+
+  // const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const swap = () => {
+    dispatch(switchOption());
+    dispatch(switchLang());
+    dispatch(switchText());
+  }
+
+  // useEffect(() => {
+  //   function handleResize() {
+  //     setScreenWidth(window.innerWidth);
+  //     if (screenWidth < 720) {
+  //       dispatch(mobile(true));
+  //     } else {
+  //       dispatch(mobile(false));
+  //     }
+  //   }
+
+  // window.addEventListener('resize', handleResize);
+  // return () => window.removeEventListener('resize', handleResize);
+
+  // }, [screenWidth, dispatch]);
+
   return (
-    <p> scan </p>
+    <div>
+      <div className="scan-container">
+        <div className="container">
+          <ScanArea />
+        </div>
+      </div>
+    </div>
   );
 }
 
